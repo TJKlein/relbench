e7dfb28296620828ae668809c6aa8929a6cc4a6d
Tassilo Klein <TJKlein@gmail.com>
2025-02-18 18:14:38 +0100

    Update __init__.py

--- relbench/datasets/__init__.py
+++ relbench/datasets/__init__.py
@@ -6,7 +6,7 @@ from typing import List
 import pooch
 
 from relbench.base import Dataset
-from relbench.datasets import amazon, avito, event, f1, hm, stack, trial
+from relbench.datasets import amazon, avito, event, f1, hm, sap, stack, trial
 
 dataset_registry = {}
 
@@ -96,5 +96,6 @@ register_dataset("rel-avito", avito.AvitoDataset)
 register_dataset("rel-event", event.EventDataset)
 register_dataset("rel-f1", f1.F1Dataset)
 register_dataset("rel-hm", hm.HMDataset)
+register_dataset("rel-sap", sap.SALTDataset)
 register_dataset("rel-stack", stack.StackDataset)
 register_dataset("rel-trial", trial.TrialDataset)
